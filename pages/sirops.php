425ebafbb18915584371f396da040f343f4fb2bc
BenzoGung <gungeranton@gmail.com>
2020-02-19 22:20:34 +0700

    prav

--- pages/sirops.php
+++ pages/sirops.php
@@ -4,7 +4,8 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=0.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-
+        <meta http-equiv="cache-control" content="no-cache">
+<meta http-equiv="expires" content="0">
 
 
         <!-- Yandex.Metrika counter -->
